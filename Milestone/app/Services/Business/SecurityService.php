ca1c1a8e98ce8f272259e2a67d27420e9a06fcee
tray15 <tray15@my.gcu.edu>
2022-06-25 13:17:00 -0700

    Update SecurityService.php
    fixed securityservice

--- Milestone/app/Services/Business/SecurityService.php
+++ Milestone/app/Services/Business/SecurityService.php
@@ -6,12 +6,6 @@ use App\Services\Data\SecurityDAO;
 
 class SecurityService
 {
-    private $dao;
-    
-    function __construct() {
-        $this->dao = new SecurityService('users');
-    }
-    
     public function login(UserModel $user) {
         $dao = new SecurityDAO();
         $query = $dao->findByUser($user);
