96237d9b78cbc2bf8a4e7951d34adb828082a266
tray15 <tray15@my.gcu.edu>
2022-06-29 11:29:00 -0700

    registration

--- Milestone/app/Services/Data/SecurityDAO.php
+++ Milestone/app/Services/Data/SecurityDAO.php
@@ -9,11 +9,13 @@ class SecurityDAO
     
     public function findByUser(UserModel $user)
     {
-        $response = DB::table('users')->where('username', $user->getUsername())->where('password', $user->getPassword())->first();
+        $response = DB::table('users')->where('username', $user->getUsername())->where('password', 
+            $user->getPassword())->first();
         return $response;
     }
     public function save(UserModel $user) {
-        $response = DB::table('users')->insert(['username' => $user->getUsername(), 'password' => $user->getPassword(), 'updated_at'=> now(), 'created_at' => now()]);
+        $response = DB::table('users')->insert(['username' => $user->getUsername(), 
+            'password' => $user->getPassword(), 'updated_at'=> now(), 'created_at' => now()]);
         return $response;
     }
 }
